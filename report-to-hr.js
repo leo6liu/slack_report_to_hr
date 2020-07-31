fd73bad58beef6f4de543cddf093f2d52060c5b3
Rocket <rocketthedogliu@gmail.com>
2020-07-31 20:23:58 +0000

    main code

--- report-to-hr.js
+++ report-to-hr.js
@@ -1,16 +1,15 @@
-// file: report-to-hr.js
+// file: slack_report_to_hr/report-to-hr.js
 //
-// description: ...
+// npm dependencies: express, xmlhttprequest
 //
 
-// set port, credentials, and app variables
+// define constants
 //
-const messageActionPort = 8080; // port to listen for message_action
-const optionsLoadPort = 8081; // port to listen for options_load
-const slackToken = 'xoxb-1214041371813-1283814382608-dTiIFmKzeTFqdBS84eoC8sDR';
-const slackSigningSecret = 'b6946a6e5bf78e0fa524f8263fbdcdce';
-const forwardChannelID = 'G017A66DXFU'; // to be deprecated
-const hrEmailAddr = 'rktliu.001@gmail.com'; // to be implemented
+const PORT = 8080; // port to listen for message_action
+const SLACK_OAUTH_TOKEN = 'xoxb-1214041371813-1283814382608-DtkAkmbK1Ufckgke8FlHcOJp';
+const SLACK_SIGNING_SECRET = 'b6946a6e5bf78e0fa524f8263fbdcdce';
+const FORWARD_CHANNEL_ID = 'G017A66DXFU'; // to be deprecated
+const HR_EMAIL_ADDRESS = 'rktliu.001@gmail.com'; // to be implemented
 
 // include modules
 //
@@ -24,54 +23,38 @@ const app = express(); // create express messageAction
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// handle report-to-hr-options-load post requests
-// expected request:
-//
-app.post('/report-to-hr-options-load', (req, res) => {
-    // print time and request body to terminal
-    //
-    console.log('\nnew report-to-hr-options-load request (' +
-		new Date().toString() + ')');
-    console.log('body: \n', req.body);
-    
-    // end response if request fails validation
-    //
-    if (validate.validateRequest(req, slackSigningSecret) == false) {
-	return res.status(400).send('Verification failed');
-    }
-
-    // close response
-    //
-    res.end();
-});
-
 // handle report-to-hr post requests
 // expected request: message_action
 //
 app.post('/report-to-hr', (req, res) => {
     // print time and request body to terminal
     //
-    console.log('\nnew report-to-hr request (' + new Date().toString() + ')');
-    console.log('body: \n', req.body);
+    console.log(`new /report-to-hr request (${new Date().toString()})`);
+    console.log(`body:\n${req.body}`);
     
-    // end response if request fails validation
+    // if validation fails, send status 400 and exit
+    // else, send status 200 and continue
     //
     if (validate.validateRequest(req, slackSigningSecret) == false) {
-	return res.status(400).send('Verification failed');
+	return (res.status(400).send('Verification failed'));
+    } else {
+	res.status(200).send();
     }
-
-    // close response
-    //
-    res.end();
     
     // extract payload from reqest body
-    // extract reporter user ID from payload
-    // extract trigger_id from payload
     //
     const payload = JSON.parse(req.body.payload);
+    //console.log(`payload:\n${payload}`);
+
+    // extract reporter's user ID and trigger_id from payload
+    //
     const reporterID = payload.user.id;
     const triggerID = payload.trigger_id;
-    console.log('payload:\n', payload);
+
+    // get report reason from user
+    //
+    let reportReason = '';
+    
     // create and send chat.getPermalink GET request
     //
     console.log('creating chat.getPermalink GET request...');
@@ -88,38 +71,6 @@ app.post('/report-to-hr', (req, res) => {
     //
     const permalink = JSON.parse(xhr_a.responseText).permalink;
 
-    // get report text
-    //
-    const reportReason = 'uhh, idk...';
-
-    /*
-    console.log('creating dialog.open POST request...');
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://slack.com/api/dialog.open', false);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + slackToken);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-	'dialog': {
-	    'callback_id': 'placeholder_001',
-	    'title': 'Report to HR',
-	    'submit_label': 'Report',
-	    'notify_on_cancel': true,
-	    'state': 'Limo',
-	    'elements': [
-		{
-		    'type': 'textarea',
-		    'label': 'pickup location',
-		    'name': 'reason_text'
-		}
-	    ]
-	},
-	'trigger_id': triggerID
-    }));
-    console.log('dialog.open response status: ', xhr.status);
-    console.log('dialog.open response text: ', xhr.responseText, '\n');
-    */
-
-    
     // format message text
     //
     const reportText = 'User <@' + reporterID + '>, ' +
@@ -131,7 +82,7 @@ app.post('/report-to-hr', (req, res) => {
     console.log('creating chat.postMessage POST request...');
     const xhr_b = new XMLHttpRequest();
     xhr_b.open('POST', 'https://slack.com/api/chat.postMessage', false);
-    xhr_b.setRequestHeader('Authorization', 'Bearer ' + slackToken);
+    xhr_b.setRequestHeader('Authorization', 'Bearer ' + SLACK_OAUTH_TOKEN);
     xhr_b.setRequestHeader('Content-Type', 'application/json');
     xhr_b.send(JSON.stringify({
 	'channel': forwardChannelID,
@@ -140,7 +91,7 @@ app.post('/report-to-hr', (req, res) => {
     console.log('chat.postMessage response status: ', xhr_b.status);
     console.log('chat.postMessage response text: ', xhr_b.responseText, '\n');
     
-    // create and send email to hrEmailAddr
+    // create and send email to HR_EMAIL_ADDRESS
     //
     
     // print status message
@@ -150,7 +101,7 @@ app.post('/report-to-hr', (req, res) => {
 
 // listen indefinitely on specified port
 //
-app.listen(messageActionPort, '0.0.0.0');
+app.listen(PORT, '0.0.0.0');
 
 //
 // end of file
